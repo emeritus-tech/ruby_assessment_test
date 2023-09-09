e2ab167712684d8891949e4074d1e3eb7cb50b0e
Anurag Mishra <anurag1998mishra@gmail.com>
2023-09-09 15:03:24 +0530

    Improved the failed test cases.

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,5 +1,6 @@
 class Enrollment < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
   belongs_to :teacher, foreign_key: :teacher_id, class_name: 'User'
+  belongs_to :student, class_name: 'User', foreign_key: :user_id
   belongs_to :program
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+    has_many :student_enrollments, foreign_key: :user_id, class_name: 'Enrollment'
+    has_many :teacher_enrollments, foreign_key: :teacher_id, class_name: 'Enrollment'
+    has_many :teachers, through: :student_enrollments, source: :teacher, class_name: 'User'
+    before_update :prevent_role_change_with_programs
+
+
+    enum kind: {
+      student: 0,
+      teacher: 1,
+      student_teacher: 2
+    }
+
+    def prevent_role_change_with_programs
+        if (student_enrollments.exists? || teacher_enrollments.exists?) && (kind == "teacher" || kind == "student")
+            errors.add(:base, "Kind can not be #{kind} because is teaching in at least one program") if kind == "student"
+            errors.add(:base, "Kind can not be #{kind} because is studying in at least one program") if kind == "teacher"
+            throw :abort
+        end
+    end
 end
